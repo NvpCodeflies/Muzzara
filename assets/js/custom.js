ef4abc2c8f9d37d0e4ade1247db11c319ce1663a
HR <naveen@codeflies.com>
2022-05-26 18:33:47 +0530

    26-may-2022 Work

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -20,4 +20,78 @@ $(document).ready(function () {
   $(".dropdown-content").click(function (e) {
     e.stopPropagation();
   });
+
+
+  // Funnel Step Form Next Functionality
+  $('.funnel_step-next-btn').click(function(){
+
+    const activeForm = $('[data-step]:visible');
+    const formStep = activeForm.attr('data-step');
+    const nextFormStep = +formStep + 1;
+    const totalSteps = $('[data-step]').length;
+
+    //Replace next button with finish button
+    if(nextFormStep == totalSteps){
+      $('.funnel_step-next-btn').hide();
+      $('.finish-btn').show();
+    }
+    else{
+      $('.funnel_step-next-btn').show();
+      $('.finish-btn').hide();
+
+    }
+
+    //Change to next form
+    activeForm.hide();
+    $(`[data-step=${nextFormStep}]`).show();
+  
+
+    //Hide prev button on first form visible
+    if((formStep + 1) == 1){
+      $('.funnel_step-prev-btn').hide();
+    }
+    else{
+      $('.funnel_step-prev-btn').show();
+    }
+  });
+
+
+  // Funnel Step Form Next Functionality
+  $('.funnel_step-prev-btn').click(function(){
+
+    const activeForm = $('[data-step]:visible');
+    const formStep = activeForm.attr('data-step');
+    const prevFormStep = +formStep - 1;
+    const totalSteps = $('[data-step]').length;
+
+    
+    //Replace next button with finish button
+    if(prevFormStep < totalSteps){
+        $('.funnel_step-next-btn').show();
+        $('.finish-btn').hide();
+      }
+      else{
+          $('.funnel_step-next-btn').hide();
+          $('.finish-btn').show();
+        }
+        
+        //Change to next form
+        activeForm.hide();
+        $(`[data-step=${prevFormStep}]`).show();
+        
+        // debugger
+
+    //Hide prev button on first form visible
+    if(prevFormStep == 1){
+      $('.funnel_step-prev-btn').hide();
+    }
+    else{
+      $('.funnel_step-prev-btn').show();
+    }
+  });
+
+  
+
+
+
 });
